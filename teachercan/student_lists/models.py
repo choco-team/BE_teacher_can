5b8f6e47c9904121758d7fbb29065702d8d3e53c
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-04-08 00:01:02 +0900

    FEAT studentList api.py에 커스텀 모델 매니저 추가

--- teachercan/student_lists/models.py
+++ teachercan/student_lists/models.py
@@ -1,7 +1,63 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+import config.exceptions as ex
 
+
+class CustomStudentListManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_student_list(self, id, user, **kwargs):
+        try:
+            student_list = self.get(id=id, user=user)
+        except ObjectDoesNotExist:
+            raise ex.not_found_student_list
+        return student_list
+    
+    def create_student_list(self, payload, user, **kwarg):
+        new_student_list = StudentList(
+            name=payload.name,
+            description=payload.description,
+            has_allergy=False,
+            is_main=not user.studentLists.count(),
+            user=user,
+        )
+        new_student_list.save()
+        return new_student_list
+    
+    def make_recent_student_list_main(self, user, **kwargs):
+        try:
+            recent_student_list = self.filter(
+                user=user, is_main=False
+            ).order_by("-updated_at")[0]
+        except ObjectDoesNotExist:
+            raise ex.not_found_student_list
+        recent_student_list.is_main = True
+        recent_student_list.save()
+    
+    def update_main_student_list(self, student_list, payload, user, **kwargs):
+        # is_main == false 를 true로 바꿀 때
+        if not student_list.is_main and payload.is_main:
+            try:
+                main_student_list = self.get(user=user, is_main=True)
+            except ObjectDoesNotExist:
+                raise ex.not_found_student_list
+            main_student_list.is_main = False
+            main_student_list.save()
+        # is_main == true 를 false로 바꿀 때
+        elif student_list.is_main and not payload.is_main:
+            self.make_recent_student_list_main(user=user)
+        student_list.is_main = payload.is_main
+        student_list.save()
+
+    def update_student_list(self, student_list, payload, user, **kwargs):
+        student_list.name = payload.name
+        student_list.description = payload.description
+        self.update_main_student_list(student_list=student_list, payload=payload, user=user)
+        student_list.has_allergy = student_list.has_allergy
+        student_list.save()
 
 class StudentList(models.Model):
+    objects = CustomStudentListManager()
     name = models.CharField(max_length=15)
     is_main = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
--- teachercan/students/models.py
+++ teachercan/students/models.py
@@ -1,4 +1,58 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+import config.exceptions as ex
+
+from teachercan.columns.models import Column
+
+
+class CustomStudentManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_student(self, id, student_list, **kwargs):
+        try:
+            student = Student.objects.get(id=id, student_list=student_list)
+        except ObjectDoesNotExist:
+            raise ex.not_found_student
+        return student
+
+    def create_student(self, payload, student_list, **kwargs):
+        new_student = Student(
+            number=payload.number, name=payload.name, gender=payload.gender, student_list=student_list
+        )
+        new_student.save()
+        return new_student
+
+    def update_student(self, payload, student_list, **kwargs):
+        student = self.get_student(id=payload.id, student_list=student_list)
+        student.number = payload.number
+        student.name = payload.name
+        student.gender = payload.gender
+        if student_list.has_allergy and student.allergy:
+            student.allergy.set([Allergy.objects.get(pk=a)
+                                for a in student.allergy])
+        for rowWithColumnId in payload.columns:
+            Row.objects.update_row(payload=rowWithColumnId, student=student)
+        student.save()
+
+
+class CustomRowManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_row(self, column, student, **kwargs):
+        try:
+            row = Row.objects.get(
+                column=column, student=student)
+        except ObjectDoesNotExist:
+            raise ex.not_found_row
+        return row
+    
+    def update_row(self, payload, student):
+        column = Column.objects.get(id=payload.id)
+        row = self.get_row(column=column, student=student)
+        row.value = payload.value
+        row.save()
+
 
 
 class Allergy(models.Model):
@@ -16,8 +70,10 @@ class StudentAllergyRelation(models.Model):
     class Meta:
         db_table = "student_allergy_set"
 
-
+    
 class Student(models.Model):
+    objects = CustomStudentManager()
+
     name = models.CharField(max_length=10)
     number = models.IntegerField()
     gender = models.CharField(
@@ -37,6 +93,7 @@ class Student(models.Model):
 
 
 class Row(models.Model):
+    objects = CustomRowManager()
     value = models.CharField(max_length=100)
     column = models.ForeignKey(
         to="columns.Column", on_delete=models.CASCADE, related_name="rows"
