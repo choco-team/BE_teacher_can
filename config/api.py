0477f1e7e599c2f580eb87becd46a18a8fcd3dff
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-04-14 19:45:42 +0900

    FEAT 명렬표에 총학생수 추가

--- config/api.py
+++ config/api.py
@@ -18,28 +18,28 @@ api = NinjaAPI(
 )
 
 
-# token인증 예외처리
-@api.exception_handler(AuthenticationError)
-def auth_unavailable(request, exc):
-    exc.code = 1001
-    exc.message = "로그인이 필요한 서비스에요."
-    exc.status_code = 401
-    return ex.error_response(request, exc, api)
+# # token인증 예외처리
+# @api.exception_handler(AuthenticationError)
+# def auth_unavailable(request, exc):
+#     exc.code = 1001
+#     exc.message = "로그인이 필요한 서비스에요."
+#     exc.status_code = 401
+#     return ex.error_response(request, exc, api)
 
 
-# 유효성검사 예외처리
-@api.exception_handler(ValidationError)
-def exception_handelr(request, exc):
-    exc.code = 1003
-    exc.message = "유효성 검사에서 문제가 발생했어요."
-    exc.status_code = 422
-    return ex.error_response(request, exc, api)
+# # 유효성검사 예외처리
+# @api.exception_handler(ValidationError)
+# def exception_handelr(request, exc):
+#     exc.code = 1003
+#     exc.message = "유효성 검사에서 문제가 발생했어요."
+#     exc.status_code = 422
+#     return ex.error_response(request, exc, api)
 
 
-# 기타 예외처리
-@api.exception_handler(Exception)
-def exception_handelr(request, exc):
-    return ex.error_response(request, exc, api)
+# # 기타 예외처리
+# @api.exception_handler(Exception)
+# def exception_handelr(request, exc):
+#     return ex.error_response(request, exc, api)
 
 
 api.add_router("/auth", auth_router)
